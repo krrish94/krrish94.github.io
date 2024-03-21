45d0d3a78348f451c76349c24568aab6197cd036
Krishna Murthy <krrish94@gmail.com>
2024-03-21 13:25:37 -0400

    minor fixes
    Signed-off-by: Krishna Murthy <krrish94@gmail.com> [ci skip]

--- _news/20210122-google-fellowship.md
+++
@@ -1,7 +0,0 @@
----
-layout: post
-date: 2021-01-22 04:00:00-0400
-inline: true
----
-
-Awarded a [Google PhD fellowship](https://research.google/outreach/phd-fellowship/) (declined)
--- assets/js/common.js
+++ assets/js/common.js
@@ -1,15 +1,1 @@
-$(document).ready(function() {
-    $('a.abstract').click(function() {
-        $(this).parent().parent().find(".abstract.hidden").toggleClass('open');
-    });
-    $('a.bibtex').click(function() {
-        $(this).parent().parent().find(".bibtex.hidden").toggleClass('open');
-    });
-    $('a.contrib').click(function() {
-        $(this).parent().parent().find(".contrib.hidden").toggleClass('open');
-    });
-    $('a.tldr').click(function() {
-        $(this).parent().parent().find(".tldr.hidden").toggleClass('open');
-    });
-    $('.navbar-nav').find('a').removeClass('waves-effect waves-light');
-});
+$(document).ready(function(){$("a.abstract").click(function(){$(this).parent().parent().find(".abstract.hidden").toggleClass("open")}),$("a.bibtex").click(function(){$(this).parent().parent().find(".bibtex.hidden").toggleClass("open")}),$("a.contrib").click(function(){$(this).parent().parent().find(".contrib.hidden").toggleClass("open")}),$("a.tldr").click(function(){$(this).parent().parent().find(".tldr.hidden").toggleClass("open")}),$(".navbar-nav").find("a").removeClass("waves-effect waves-light")});
